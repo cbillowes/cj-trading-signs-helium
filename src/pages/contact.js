caaa77bc6ae3000b36a05214ff2217930c8b561a
Clarice Bouwer <cbillowes@gmail.com>
2019-10-14 04:02:22 +0200

    Add Contact Us page

---
+++ src/pages/contact.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title={`Contact Us`} />
+    <h1>Contact Us</h1>
+    <ul>
+      <li class="whatsapp">067 259 4314</li>
+      <li class="mobile">067 259 4314</li>
+      <li class="email">cj.trading.signs@gmail.com</li>
+      <li class="website">www.cj-trading-signs.com</li>
+      <li class="facebook"><a href="https://www.facebook.com/CJ.McAughey/?eid=ARBGrQnsG6W76wyaBwuCPbK_DVtFonPCAx9Odej8ZcBw_RogbTIdzTIhHkQ_8ZZXOMHvHwDyQhJzsS3K">Facebook</a></li>
+    </ul>
+  </Layout>
+)
+
+export default ContactPage
