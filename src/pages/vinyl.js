3a242b9cc866ab92011047571ea07e734564d8ba
Clarice Bouwer <cbillowes@gmail.com>
2019-10-14 03:53:18 +0200

    Add Vinyl Stickers & Safety Signs page

---
+++ src/pages/vinyl.js
@@ -0,0 +1,41 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const CopiesPage = () => (
+  <Layout>
+    <SEO title={`Vinyl Stickers & Safety Signs`} />
+    <h1>Vinyl Stickers &amp; Safety Signs</h1>
+    <ul>
+      <li>We stock 3 and 8 year Vinyl.</li>
+      <li>3 Year used mainly for indoors or short term outdoors.</li>
+      <li>8 Year Vinyl for use outdoors.</li>
+      <li>A wide variety of colours available.</li>
+      <li>Frosted Glass and clear also available.</li>
+      <li>
+        Design colours are limited as each colour must be cut separately the
+        assembled.
+      </li>
+      <li>
+        Long term Signs can be mounted on ABS plastic, Chromadek, or Aluminium
+        boards.
+      </li>
+      <li>
+        Short term signs on Masonite, corrugated cardboard or corrugated
+        plastic.
+      </li>
+      <li>
+        From Toilet signage to advertising your sales prices to bumper stickers.
+      </li>
+    </ul>
+    <h2>Stay OSH act compliant</h2>
+    <p>ABS, Chromadek, Aluminium, corrugated plastic or board</p>
+    <ul>
+      <li>Sublimation on Aluminium</li>
+      <li>All safety signage.</li>
+      <li>All informative signage</li>
+    </ul>
+  </Layout>
+)
+
+export default CopiesPage
