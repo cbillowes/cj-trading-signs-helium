a720896858760fb8513704ce4c793c57ab28c3da
Clarice Bouwer <cbillowes@gmail.com>
2019-10-14 03:52:35 +0200

    Add Mugs, Plates, Shirts & Caps page

---
+++ src/pages/mugs.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const CopiesPage = () => (
+  <Layout>
+    <SEO title={`Mugs, Plates, Shirts & Caps`} />
+    <h1>Mugs, Plates, Shirts &amp; Caps</h1>
+    <p>
+      We can print your personalised designs on Shirts, Puzzles,Mugs, Plates,
+      Golf Caps. Aprons, etc.
+    </p>
+    <p>
+      Come to CJ Trading &amp; Signs where we are in a security boom controlled
+      Estate with parking on the premises.
+    </p>
+    <ul>
+      <li>What is printed is totally up to you.</li>
+      <li>
+        Ideas can be to print a photo, your company branding, a logo or your
+        slogan.
+      </li>
+      <li>
+        Caps you can choose between full colour transfers or max 3 colour heat
+        applied vinyl.{" "}
+      </li>
+      <li>Puzzles are personalised but only in A4 with big or small pieces.</li>
+      <li>High Visibility vests, branded with designation and names.</li>
+    </ul>
+  </Layout>
+)
+
+export default CopiesPage
