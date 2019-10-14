8e96cdc1fd40f5e743ed73bac66b22c05c703a9f
Clarice Bouwer <cbillowes@gmail.com>
2019-10-14 03:32:47 +0200

    Add Copies & Printing page

---
+++ src/pages/copies.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title={`Copies & Printing`} />
+    <h1>Copies &amp; Printing</h1>
+    <p>
+      There are so many people who do not need to own a computer, colour
+      printer, copier or laminator because they hardly ever need to use one, but
+      when the need arises where do you go?
+    </p>
+    <p>
+      Come to CJ Trading &amp; Signs where we are in a security boom controlled
+      Estate with parking on the premises.
+    </p>
+    <ul>
+      <li>
+        Type out your Documents that are emailed, on memory stick or even neatly
+        &amp; clearly hand written (obviously we must be able to read it easily).
+        Type your CV.
+      </li>
+      <li>Make photocopies in B/W or full colour.</li>
+      <li>Scan and email documents for you.</li>
+      <li>Single &amp; double sided scanning and printing.</li>
+      <li>Laminate your valuable documents.</li>
+      <li>All only up to A4.</li>
+    </ul>
+  </Layout>
+)
+
+export default SecondPage
