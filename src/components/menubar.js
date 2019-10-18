85a6075b0475ad1f31055caee1d96947ea6a5d9a
Clarice Bouwer <cbillowes@gmail.com>
2019-10-18 23:29:39 +0200

    Use browser monads to determine window object and refactor component

--- src/components/menubar.js
+++ src/components/menubar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { window } from "browser-monads"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import _ from "lodash"
 import "./menubar.css"
@@ -7,10 +8,9 @@ class Menubar extends Component {
   constructor(props) {
     super(props)
 
-    const windowContext = this.getWindowContext(props)
-    const pathSegment = this.getPathSegment(
-      this.getPathname(windowContext)
-    )
+    const windowContext = this.props.window ? this.props.window : window
+    const pathname = windowContext.location.pathname
+    const pathSegment = this.getPathSegment(pathname)
     this.state = {
       navigation: props.navigation,
       toggle: props.toggle,
@@ -18,54 +18,37 @@ class Menubar extends Component {
     }
   }
 
-  getWindowContext = (props) => {
-    const custom = (props.window) 
-      ? props.window
-      : {
-        location: {
-          pathname : "/"
-        }
-      }
-    return (typeof window === "undefined") ? custom : window
+  getPathSegment = pathname => {
+    if (pathname !== "/") {
+      const pathSegments = pathname.split("/")
+      return `/${pathSegments[1]}`
+    }
+    return pathname
   }
 
-  getPathname = (context) => {
-    return (typeof context.location === "undefined") 
-      ? "/" 
-      : context.location.pathname
-  }
-
-  getPathSegment = (pathname) => {
-     if (pathname !== "/") {
-       const pathSegments = pathname.split("/")
-       return `/${pathSegments[1]}`
-     }
-     return pathname
-  }
-
-  isSelected = (url) => {
+  isSelected = url => {
     return this.state.pathSegment === url
   }
 
   render() {
     return (
-      <header
-        className={`menubar ${this.props.opened ? "opened" : "closed"}`}>
-          <ul>
-            {this.state.navigation.map((item) => {
-              return (
-                <li
-                  key={_.kebabCase(item.name)}
-                  className="menuitem">
-                    <Link 
-                      className={`${this.isSelected(item.url) ? "selected" : ""}`}
-                      to={item.url}>{item.name}</Link>
-                </li>
-              )
-            })}
-          </ul>
+      <header className={`menubar ${this.props.opened ? "opened" : "closed"}`}>
+        <ul>
+          {this.state.navigation.map(item => {
+            return (
+              <li key={_.kebabCase(item.name)} className="menuitem">
+                <Link
+                  className={`${this.isSelected(item.url) ? "selected" : ""}`}
+                  to={item.url}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
       </header>
-    ) 
+    )
   }
 }
 
