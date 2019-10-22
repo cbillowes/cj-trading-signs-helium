31fb179c2d693f0075d14ef01005de1af469c85c
Clarice Bouwer <cbillowes@gmail.com>
2019-10-22 06:39:09 +0200

    Boxify the contact us page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,15 +8,24 @@ const ContactPage = () => (
     <SEO title={`Contact Us`} />
     <Squash>
       <h1>Contact Us</h1>
-      <ul>
-        <li className="whatsapp">067 259 4314</li>
-        <li className="mobile">067 259 4314</li>
-        <li className="email">cj.trading.signs@gmail.com</li>
-        <li className="website">www.cj-trading-signs.com</li>
-        <li className="facebook">
-          <a href="https://www.facebook.com/CJ.McAughey/?eid=ARBGrQnsG6W76wyaBwuCPbK_DVtFonPCAx9Odej8ZcBw_RogbTIdzTIhHkQ_8ZZXOMHvHwDyQhJzsS3K">
-            Facebook
-          </a>
+      <ul className="boxes">
+        <li className="c">
+          <strong>Call or Whatsapp us.</strong><br/>
+          <a href="tel:+27672594314">067 259 4314</a>
+        </li>
+        <li className="y">
+          <strong>Pop us an email.</strong><br/>
+          <a href="mailto:cj.trading.signs@gmail.com">cj.trading.signs@gmail.com</a>
+        </li>
+        <li className="m">
+          <strong>Visit and share our website.</strong><br/>
+          <a href="https://www.cj-trading-signs.com">www.cj-trading-signs.com</a>
+        </li>
+        <li className="k">
+          <strong>Like us on Facebook.</strong><br/>
+          <a href="https://www.facebook.com/CJ.McAughey">
+            CJ.McAughey
+          </a> 
         </li>
       </ul>
     </Squash>
