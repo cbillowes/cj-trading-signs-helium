346eece4bad3643bdaaa0ea4000ea4cce7a32eb0
Clarice Bouwer <cbillowes@gmail.com>
2019-10-24 04:19:27 +0200

    Revise product descriptions

--- src/data/Products.js
+++ src/data/Products.js
@@ -3,19 +3,19 @@ module.exports = [
     name: "Business Cards",
     image: "business-cards.png",
     description:
-      "We will make those small quantity orders if that is what you require. You can choose a matt or gloss finish."
+      "Available in a matt or gloss image. We do small and large quantity orders."
   },
   {
     name: "Mouse Pads",
     image: "mousepads.png",
     description:
-      "Foam mouse pads with any design you like be it logos with company details, advertising, kiddies and adult themes."
+      "Print your custom designs on foam mouse pads. Ideas include your company logo, details, advertising, kiddies and adult themes."
   },
   {
     name: "Mugs",
     image: "mugs.png",
     description:
-      "Design your own coffee mugs, plain white or coloured inside with anything you desire printed on the outside. In one area or all the way around the mug."
+      "Design your own coffee mugs. Options include plain white or coloured cups. Print in one area or all the way around the mug."
   },
   {
     name: "Heat Change Mugs",
@@ -26,17 +26,18 @@ module.exports = [
     name: "Fabrics",
     image: "tshirts.png",
     description:
-      "Anything you want on a T-Shirt, Cap, Shirt, Pillow Case, Apron etc. Print in Vinyl, Glitter, Flock or Heat Transfer Paper."
+      "Print anything you want on a t-shirt, cap, shirt, pillow case, apron etc. Print in Vinyl, Glitter, Flock or Heat Transfer Paper."
   },
   {
     name: "Clocks",
     image: "clocks.png",
     description:
-      "Clocks are made from board or fibre. They come with mechanism and hands. Print whatever you like on them. Uses 1 AA battery which is not included."
+      "Clocks are made from board or fibre. They come with a mechanism and hands. Print whatever you like on them. Uses 1 AA battery which is not included."
   },
   {
     name: "Clock Hands",
     image: "clockhands.png",
+    orientation: "landscape",
     description:
       "Hands come with your clock. We will assemble it after printing."
   },
@@ -44,52 +45,53 @@ module.exports = [
     name: "Plates and Stands",
     image: "plates.png",
     description:
-      "Make a memory plate reminding you of a special occasion, wedding, anniversaries, holiday destinations, any photo can be printed on your plate."
+      "Make a memory plate reminding you of a special occasion, wedding, anniversaries, holiday destinations. Essentially anything can be printed on your plate."
   },
   {
     name: "Coasters",
     image: "coasters.png",
+    orientation: "landscape",
     description:
-      "Customise your own coasters (in board or foam) or give them out as gifts. Photos, text, comics, animals, places of interest, almost anything can be printed on them."
+      "Customise your own coasters (in board or foam) or give them out as gifts. Ideas include photos, text, comics, animals, places of interest."
   },
   {
     name: "Mirrors",
     image: "mirror.png",
-    description: "Have a picture of your child or partner framed by a mirror."
+    description: "Have a picture printed and framed on a mirror. Ideas include a photo of your child or partner."
   },
   {
     name: "Wine Bottle Holders",
     image: "wine-bottle-holder.png",
-    description: "Perfect gift with your details printed on the front."
+    description: "A perfect gift that can have custom artwork printed on the front of it."
   },
   {
     name: "Jam Jars",
     image: "jars.png",
     description:
-      "Do you have a pub or bar? Have your own logo, slogan or picture printed on the jar."
+      "Do you have a pub or bar? Have your own logo, slogan or picture printed on the jar to expose your brand."
   },
   {
     name: "Water Bottles",
     image: "water-bottle.png",
     description:
-      "Aluminium screw-top, with clip, water bottle iwth your personal design printed on it."
+      "Print your custom designs on an aluminium screw-top with clip water bottle. Show off your brand or simply personalise it."
   },
   {
     name: "Personalised Lighters",
     image: "lighter.png",
     description:
-      "A perfect gift for a smoker or anyone who needs a light for a braai. Personalise as you desire."
+      "Light up somebody's life. A perfect gift for a smoker or show off at a braai. Personalise as you desire."
   },
   {
     name: "Puzzles",
     image: "puzzles-rect.png",
     description:
-      "Photos of your children or animals or places your children have visited with small and large pieces."
+      "We offer small and large piece puzzles. Printing ideas include photos of your children, pets, places your children have visited, vehicles, special occasions or comic designs."
   },
   {
     name: "Shaped Puzzles",
     image: "puzzles.png",
     description:
-      "A photo of your child or even your pet or vehicle with very big pieces for small children or a heart shaped puzzle with beautiful picture of you and your partner at a very special occasion."
+      "We offer heart shaped puzzles essential for gifts and something different for kiddies. Printing ideas include photo of your children, pets, places your children have visited, vehicles, special occasions or comic designs."
   }
 ]
